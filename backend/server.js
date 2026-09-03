c111e503cb948c414fa1b5f380a90096fba2c35a
Zakir Rehman Mala <zakirmala099@gmail.com>
2026-09-04 01:12:38 +0500

    vercel 5

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 import express from "express";
 
 const app = express();
-
+app.use(cors({
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://fastfood-backend-v1.vercel.app"],
+    credentials: true
+}));
+app.options(/.*/, cors()); // yeh bhi rakho preflight ke liye
+//middleware
+app.use(express.json())
 app.get("/", (req, res) => {
     res.send("Vercel API Working");
 });
- 
+
 export default app;
