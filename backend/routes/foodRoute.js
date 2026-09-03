ebbc23cbaef43ed451092e2e1f1fec2813bad10d
Zakir Rehman Mala <zakirmala099@gmail.com>
2026-09-04 01:48:09 +0500

    v14

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,22 +1,37 @@
-import express from 'express'
-import { addFood, listFood, removeFood, editFood } from '../controllers/foodController.js';
-// create image storage system 
-import multer from 'multer';
+// import express from 'express'
+// import { addFood, listFood, removeFood, editFood } from '../controllers/foodController.js';
+// // create image storage system 
+// import multer from 'multer';
 
-const foodRouter = express.Router()
+// const foodRouter = express.Router()
 
-//image storage engine 
-const storage = multer.diskStorage({
-    destination:"uploads",
-    filename:(req,file,cb)=>{
-        return cb(null,`${Date.now()}${file.originalname}`)
-    } 
-})
-const upload = multer({storage:storage})
-foodRouter.post("/add",upload.single("image"),addFood)
-foodRouter.get("/list",listFood)
-foodRouter.post("/remove",removeFood)
-foodRouter.put("/edit", editFood);
+// //image storage engine 
+// const storage = multer.diskStorage({
+//     destination:"uploads",
+//     filename:(req,file,cb)=>{
+//         return cb(null,`${Date.now()}${file.originalname}`)
+//     } 
+// })
+// const upload = multer({storage:storage})
+// foodRouter.post("/add",upload.single("image"),addFood)
+// foodRouter.get("/list",listFood)
+// foodRouter.post("/remove",removeFood)
+// foodRouter.put("/edit", editFood);
 
   
+// export default foodRouter;
+
+
+import express from "express";
+import { listFood} from '../controllers/foodController.js';
+
+const foodRouter = express.Router();
+foodRouter.get("/list",listFood)
+foodRouter.get("/test", (req, res) => {
+    res.json({
+        success: true,
+        message: "Food router working"
+    });
+});
+
 export default foodRouter;
