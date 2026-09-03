14da9e3da6e295115b1ac360a29b2f4c96ab25dc
Zakir Rehman Mala <zakirmala099@gmail.com>
2026-09-04 01:44:35 +0500

    v13

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import userRouter from "./routes/userRoute.js";
-import foodRouter from "./routes/foodRoute.js";
+// import foodRouter from "./routes/foodRoute.js";
 const app = express();
 app.use(cors({
     origin: ["http://localhost:5173", "http://localhost:5174", "https://fastfood-backend-v1.vercel.app"],
