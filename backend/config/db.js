ddf13d3c8ad473df23eabfe3eb6565374a5ff2d4
Zakir Rehman Mala <zakirmala099@gmail.com>
2026-09-04 00:40:46 +0500

    error solve vercel

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,11 @@ import config from "./config.js";
 
 export const connectDB = async () => {
   try {
-    console.log('mongodbUrl',config.mongodbUrl);
+    console.log('mongodbUrl', config.mongodbUrl);
     await mongoose.connect(config.mongodbUrl);
     console.log("✅ DB Connected");
   } catch (error) {
     console.log("❌ DB Connection Failed:", error.message);
+    throw error;
   }
 };
