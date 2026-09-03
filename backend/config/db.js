ec272eb9d420b3827491b65b510ed7f7f7844980
Zakir Rehman Mala <zakirmala099@gmail.com>
2026-09-04 01:21:42 +0500

    ver 8

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,27 @@
+// import mongoose from "mongoose";
+// import config from "./config.js";
+
+// export const connectDB = async () => {
+//   try { 
+//     await mongoose.connect(config.mongodbUrl);
+//     console.log("✅ DB Connected");
+//   } catch (error) {
+//     console.log("❌ DB Connection Failed:", error.message);
+//     throw error;
+//   }
+// };
 import mongoose from "mongoose";
 import config from "./config.js";
 
 export const connectDB = async () => {
-  try { 
-    await mongoose.connect(config.mongodbUrl);
-    console.log("✅ DB Connected");
-  } catch (error) {
-    console.log("❌ DB Connection Failed:", error.message);
-    throw error;
-  }
+    try {
+        console.log("MODE:", process.env.MODE);
+        console.log("MongoDB URL exists:", config.mongodbUrl);
+
+        await mongoose.connect(config.mongodbUrl);
+
+        console.log("✅ DB Connected");
+    } catch (error) {
+        console.error("❌ DB Connection Failed:", error.message);
+    }
 };
