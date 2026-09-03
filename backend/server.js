2b93058c1d04d87bf000784f79b1ba19c6266bdd
Zakir Rehman Mala <zakirmala099@gmail.com>
2026-09-04 01:19:03 +0500

    ver 8

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
 const app = express();
 app.use(cors({
     origin: ["http://localhost:5173", "http://localhost:5174", "https://fastfood-backend-v1.vercel.app"],
@@ -10,7 +11,7 @@ app.options(/.*/, cors()); // yeh bhi rakho preflight ke liye
 app.use(express.json())
 
 //DB CONNECTION
-connectDB(); 
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("Vercel API Working");
